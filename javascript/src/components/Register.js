247ed176e34d873dc99c0d7ecf806abccbb56fe8
XandalfTheGray <xapicella7@gmail.com>
2023-11-19 14:05:19 -0500

    feat: Add user registration component with backend integration

---
+++ javascript/src/components/Register.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios'; // Ensure axios is installed
+
+function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Basic validation for password match
+    if (password !== confirmPassword) {
+      setError('Passwords do not match.');
+      return;
+    }
+
+    try {
+      // API call to register the user
+      const response = await axios.post('http://localhost:3000/api/register', { email, password });
+      console.log('User registered:', response.data);
+      // Handle success here, such as redirecting to the login page or showing a success message
+    } catch (err) {
+      // Handle errors here, such as displaying a message to the user
+      setError(err.response?.data?.message || 'Registration failed');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
