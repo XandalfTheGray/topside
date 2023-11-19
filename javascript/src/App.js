99ee0668f5977792064ea59d2647ea3f9c23c787
XandalfTheGray <xapicella7@gmail.com>
2023-11-19 15:26:31 -0500

    feat: Add default route redirection to /login in App.js
    - Implement a default route in App.js that redirects the root path to the /login route.
    - Ensure users land on a defined page even when accessing the base URL.

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import User from './components/User';
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/user" element={<User />} />
