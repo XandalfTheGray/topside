b7951dc2461668182b25daeefa6b782a7593121d
XandalfTheGray <xapicella7@gmail.com>
2023-11-19 15:56:48 -0500

    feat: Enhance Login component with error handling and registration link
    - Implement error handling in the login process.
    - Add a link to the Register component for user convenience.

--- javascript/src/components/Login.js
+++ javascript/src/components/Login.js
@@ -1,39 +1,53 @@
-// Import necessary libraries
-import React, { useState } from 'react'; // useState is a Hook that lets you add React state to function components
-import axios from 'axios'; // axios is a library for making HTTP requests
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-// Define the Login component
 function Login() {
-  // Create state variables for username and password
-  const [username, setUsername] = useState(''); // The initial value is an empty string
-  const [password, setPassword] = useState(''); // The initial value is an empty string
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState('');
 
-  // Define the function to be called when the form is submitted
   const handleSubmit = async (event) => {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault();
+    try {
+      // Replace with your API endpoint as necessary
+      const response = await axios.post('http://localhost:3000/api/login', { username, password });
+      
+      // Handle successful login
+      // You might want to redirect the user or save the login data
+      console.log('Login successful', response.data);
 
-    // Send a POST request to '/api/login' with the username and password as data
-    const response = await axios.post('/api/login', { username, password });
-
-    // TODO: Handle the response. This might involve setting more state variables,
-    // redirecting the user to another page, or other side effects.
+      // TODO: Redirect to another page or update the state as necessary
+    } catch (error) {
+      // Handle errors, such as showing login failure message
+      setLoginError('Login failed. Please check your credentials.');
+      console.error('Login error:', error);
+    }
   };
 
-  // Render the component
   return (
-    // When the form is submitted, call the handleSubmit function
-    <form onSubmit={handleSubmit}>
-      {/* When the value of the input field changes, update the username state variable */}
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-
-      {/* When the value of the input field changes, update the password state variable */}
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-
-      {/* This button submits the form */}
-      <button type="submit">Log In</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Log In</button>
+      </form>
+      {loginError && <p style={{ color: 'red' }}>{loginError}</p>}
+      <p>
+        Don't have an account? <Link to="/register">Register here</Link>
+      </p>
+    </div>
   );
 }
 
-// Export the Login component so it can be imported and used in other files
 export default Login;
