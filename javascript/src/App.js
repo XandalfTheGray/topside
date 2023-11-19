5f4f6ab4f5a818e36f19717b8e1c2eee93e13760
XandalfTheGray <xapicella7@gmail.com>
2023-11-19 15:18:24 -0500

    fix: Update routing to comply with react-router-dom v6
    - Replace Switch with Routes in App.js.
    - Modify Route components to use element prop for react-router-dom v6 compatibility.

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import User from './components/User';
@@ -7,12 +7,12 @@ import User from './components/User';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/user" component={User} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/user" element={<User />} />
         {/* add more routes as needed */}
-      </Switch>
+      </Routes>
     </Router>
   );
 }
