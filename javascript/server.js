ac18ee274aee968fd5c9c1e2bbc64ebe171abc8a
XandalfTheGray <xapicella7@gmail.com>
2023-11-19 14:03:24 -0500

    Created the full server.js code with reg and login endpoints and password handling
    - bcrypt for password hashing.
    - An in-memory database (users object) for storing user email and hashed password.
    - Registration Endpoint (/api/register) to register a new user. It checks if the user already exists, hashes the password, and stores the user.
    Login Endpoint (/api/login) to log in a user. It checks if the user exists and compares the hashed password.
    Remember, this is a basic implementation suitable for learning or testing. For a real-world application, you should use a proper database, add more robust error handling, and validate user input thoroughly.

--- javascript/server.js
+++ javascript/server.js
@@ -1,13 +1,52 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const app = express();
 
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 
-app.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
-  // TODO: Check the username and password against your database
-  // If they're valid, send a response like { status: 'success' }
-  // If they're not valid, send a response like { status: 'error' }
+// Simulated in-memory database
+const users = {}; // Key: email, Value: password hash
+
+app.post('/api/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if the user already exists
+  if (users[email]) {
+    return res.status(400).json({ status: 'error', message: 'User already exists' });
+  }
+
+  try {
+    // Hash the password before storing it
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt rounds
+
+    // Store the new user
+    users[email] = hashedPassword;
+
+    res.status(201).json({ status: 'success', message: 'User registered' });
+  } catch (err) {
+    res.status(500).json({ status: 'error', message: 'Internal server error' });
+  }
+});
+
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if the user exists
+  if (!users[email]) {
+    return res.status(400).json({ status: 'error', message: 'User does not exist' });
+  }
+
+  try {
+    // Compare the hashed password with the one stored in our database
+    const isMatch = await bcrypt.compare(password, users[email]);
+    if (isMatch) {
+      res.json({ status: 'success', message: 'Logged in successfully' });
+    } else {
+      res.status(400).json({ status: 'error', message: 'Invalid password' });
+    }
+  } catch (err) {
+    res.status(500).json({ status: 'error', message: 'Internal server error' });
+  }
 });
 
 app.listen(3000, () => {
