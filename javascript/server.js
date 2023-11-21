fa1198acb984c609698b0d80ac3da36fbe335b16
XandalfTheGray <xapicella7@gmail.com>
2023-11-20 23:36:31 -0500

    fix: changed last few lines of server.js to make it run on port 3001 instead of 3000 (where frontend was running)

--- javascript/server.js
+++ javascript/server.js
@@ -94,6 +94,6 @@ app.get('/api/user/:email', async (req, res) => {
 });
 
 // Server Listening on Port 3000
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
 });
